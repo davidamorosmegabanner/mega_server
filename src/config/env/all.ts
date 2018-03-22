cde1813755d2f800ecac37137feeb9460f26b75b
marctorrelles <marctorrelles@gmail.com>
2018-03-22 19:15:09 +0100

    4 Gb as max upload size of a creativity

--- src/config/env/all.ts
+++ src/config/env/all.ts
@@ -1,5 +1,5 @@
 export default {
-    maxFileSize: 200 * 1024 * 1024, // 200 Mb
+    maxFileSize: 4 * 1024 * 1024 * 1024, // 4 Gb
     passwordSalt: "miKjM6qp6WTpYGwZ87ksaaS64TBB02VTWET5zQDjHqcRv6sYc1A99ZV0ybJzCKr9",
     serverToken: "LQuJFAjR2rFC5piTovYB1mIfMQ0HIw2D26BkuTCMrtFwa5RNgZNLR6nWBt6JxKbG",
 };
